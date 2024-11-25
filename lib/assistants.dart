be03cd9280691a1bae2d95966f2b72ada3dd34fd
Annika <annika.lenneper@smail.th-koeln.de>
2024-11-25 15:45:38 +0100

    corrected spelling

---
+++ lib/assistants.dart
@@ -0,0 +1,23 @@
+
+
+class Assistant {
+  var name = '';
+  var contractedHours = 0.0;
+  var actualHours = 0.0;
+  final List<double> surchargeCounter = List.empty();
+  final List<String> notes = List.empty();
+
+  Assistant(this.name);
+
+  void updateActualHours() {
+    //TO-DO: Implement this method
+  }
+
+  void updateSurchargeCounter() {
+    //TO-DO: Implement this method
+  }
+
+  void addNote() {
+    //TO-DO: Implement this method
+  }
+}
