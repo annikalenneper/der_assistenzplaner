ed5ba0a0f71478182f8cc769f9719fc75611d4f3
Annika <annika.lenneper@smail.th-koeln.de>
2024-11-26 14:21:29 +0000

    changed more methods to inline functions

--- lib/models/assistant.dart
+++ lib/models/assistant.dart
@@ -24,38 +24,16 @@ class Assistant {
   set contractedHours(double contractedHours) => (contractedHours > 0) ? _contractedHours = contractedHours : throw ArgumentError('contractedHours darf nicht negativ sein.');
   set actualHours(double actualHours) => (actualHours > 0) ? _actualHours = actualHours : throw ArgumentError('actualHours darf nicht negativ sein.');
 
-  void addNote(String title, String text) {
-    //TO-DO: Implement checks for valid values for title and text
-    _notes.add(Note(title, text));
-  }
-
-  void removeNoteByIndex(int index) {
-    if (index < 0 || index >= _notes.length) {
-      throw RangeError('Index $index out of bounds for notes list.');
-    }
-    _notes.removeAt(index);
-  }
-
-  void assignTag(Tag tag) {
-    ///make check unneccessary by implementing tag assignment UI in a way, that only tags that are not already assigned can be selected 
-    if (_tags.contains(tag)) {
-      throw ArgumentError('Tag $tag bereits zugeordnet.');
-    }
-    _tags.add(tag);
-  }
-
-  void removeTagByIndex(int index) {
-    if (index < 0 || index >= _tags.length) {
-      throw RangeError('Index $index out of bounds for tags list.');
-    }
-    _tags.removeAt(index);
-  }
+  void addNote(String title, String text) => _notes.add(Note(title, text)); //TO-DO: Implement checks for valid values for title and text
+  void removeNotebyIndex(int index) => (index < 0 || index >= _notes.length) ? throw RangeError('Index $index out of bounds for notes list.') : _notes.removeAt(index);
+  
+  ///make check unneccessary by implementing tag assignment UI in a way, that only tags that are not already assigned can be selected 
+  void assignTag(Tag tag) => (_tags.contains(tag)) ? throw ArgumentError('Tag $tag bereits zugeordnet.') : _tags.add(tag); 
+  void removeTagByIndex(int index) => (index < 0 || index >= _tags.length) ? throw RangeError('Index $index out of bounds for tags list.') : _tags.removeAt(index);
 
   @override
-  String toString() {
-    //TODO: Implement this method
-    return super.toString();
-  }
+  String toString() => 'Assistant: $_name'; //TO-DO: adjust to show all relevant information;
+  
 }
 
 
