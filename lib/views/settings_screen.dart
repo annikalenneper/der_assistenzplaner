b3f4a129fdac83839fc30fdd5898b466c2c301a5
Annika <annika.lenneper@smail.th-koeln.de>
2024-12-06 13:20:09 +0000

    implemented simple UI outputs for tag testing

--- lib/views/settings_screen.dart
+++ lib/views/settings_screen.dart
@@ -1,11 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:der_assistenzplaner/viewmodels/tag_model.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
 
 ///SettingsScreen
 class SettingsScreen extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Einstellungen'),
+      child: ElevatedButton(
+        child: Text('Tags'),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => TagSettings()),
+          );
+        },
+      )
     );
   }
 }
+
+class TagSettings extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var tagModel = Provider.of<TagModel>(context);
+    return Scaffold(
+      appBar: AppBar(title: Text('Tags')),
+      body: Center(
+        child: ListView.builder(
+          itemCount: tagModel.exampleTags.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: FaIcon(tagModel.exampleTags[index].tagSymbol.icon, size: 40),
+              title: Text(tagModel.exampleTags[index].name),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
