37d69491c1c1afb165706ba8d8914a62009ed0e1
Annika <annika.lenneper@smail.th-koeln.de>
2024-12-09 14:45:35 +0000

    added navigation utility functions for assistant screen transitions

---
+++ lib/utils/nav.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:der_assistenzplaner/views/assistant_screen.dart';
+import 'package:der_assistenzplaner/main.dart';
+
+/// remove stack and navigate 
+void navigateToAssistantScreen(BuildContext context) {
+  Navigator.pushNamedAndRemoveUntil(
+    context,
+    '/assistantScreen',
+    (route) => false
+  );
+}
+
+/// replaces current Route, stack won't change
+void navigateToAssistantDetails(BuildContext context, assistant) {
+  Navigator.pushReplacement(
+    context,
+    MaterialPageRoute(builder: (context) => AssistantDetails(assistant)),
+  );
+}
