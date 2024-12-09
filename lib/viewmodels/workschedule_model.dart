c5872f4f4e3bdcb9832562987fb9c87ec34d84c7
Annika <annika.lenneper@smail.th-koeln.de>
2024-12-09 14:47:08 +0000

    simplified constructor

--- lib/viewmodels/workschedule_model.dart
+++ lib/viewmodels/workschedule_model.dart
@@ -1,22 +1,25 @@
 import 'package:der_assistenzplaner/models/shift.dart';
 import 'package:der_assistenzplaner/models/workschedule.dart';
 import 'package:flutter/material.dart';
+import 'package:der_assistenzplaner/test_data.dart';
 
 class WorkscheduleModel extends ChangeNotifier {
-  final Workschedule _workschedule;
+  Workschedule workschedule = createTestWorkSchedule();
 
-  WorkscheduleModel(this._workschedule);
+  WorkscheduleModel();
 
-  get start => _workschedule.start;
-  get end => _workschedule.end;
-  get shifts => _workschedule.scheduledShifts;
+  get start => workschedule?.start;
+  get end => workschedule?.end;
+  get shifts => workschedule?.scheduledShifts;
 
   List<ScheduledShift> getScheduledShiftsByDay(DateTime day) {
-    return _workschedule.getScheduledShiftsByDay(day);
+    return workschedule.getScheduledShiftsByDay(day);
   }
 
   void addShift(ScheduledShift shift) {
-    _workschedule.addShift(shift);
+    workschedule.addShift(shift);
     notifyListeners(); 
   }
+
+  /// needs no database, scheduledShifts will be stored in database
 }
