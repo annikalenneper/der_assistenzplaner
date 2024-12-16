c6a552aea4f94d2c03478c7afc62bbaa6e94facf
Annika <annika.lenneper@smail.th-koeln.de>
2024-12-16 18:59:57 +0000

    added comments for missing implementations

--- lib/viewmodels/workschedule_model.dart
+++ lib/viewmodels/workschedule_model.dart
@@ -1,25 +1,30 @@
+import 'package:der_assistenzplaner/models/assistant.dart';
 import 'package:der_assistenzplaner/models/shift.dart';
 import 'package:der_assistenzplaner/models/workschedule.dart';
+import 'package:der_assistenzplaner/viewmodels/shift_model.dart';
 import 'package:flutter/material.dart';
 import 'package:der_assistenzplaner/test_data.dart';
 
+
+/// WorkscheduleModel holds list of scheduled shifts and notes
+/// will also be used for generating work schedule and export functions
+/// needs no database, uses scheduledShifts from scheduledShiftBox (hive database)
 class WorkscheduleModel extends ChangeNotifier {
   Workschedule workschedule = createTestWorkSchedule();
+  List<Note> notes = [];
 
   WorkscheduleModel();
 
   get start => workschedule.start;
   get end => workschedule.end;
-  get shifts => workschedule.scheduledShifts;
+
+  /// workschedule = list of scheduled shifts
+  List<ScheduledShift> getWorkschedule(DateTime start, DateTime end, ShiftModel shiftModel) => 
+    shiftModel.getScheduledShiftsByDayRange(start, end) as List<ScheduledShift>;
+  
 
   List<ScheduledShift> getScheduledShiftsByDay(DateTime day) {
     return workschedule.getScheduledShiftsByDay(day);
   }
 
-  void addShift(ScheduledShift shift) {
-    workschedule.addShift(shift);
-    notifyListeners(); 
-  }
-
-  /// needs no database, scheduledShifts will be stored in database
 }
