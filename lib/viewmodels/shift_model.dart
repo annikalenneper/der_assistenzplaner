fa37c79606ba745c834007b60e9bfeb2677c1d9d
Annika <annika.lenneper@smail.th-koeln.de>
2024-12-16 14:52:45 +0000

    implement getters, setters and database methods for shifts

--- lib/viewmodels/shift_model.dart
+++ lib/viewmodels/shift_model.dart
@@ -1,8 +1,5 @@
 
-
-
 import 'dart:developer';
-
 import 'package:der_assistenzplaner/models/shift.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
@@ -10,11 +7,38 @@ import 'package:hive/hive.dart';
 
 class ShiftModel extends ChangeNotifier {
   late Box<Shift> _shiftBox;
-  Shift? currentshift;
+  late Box<ScheduledShift> _scheduledShiftBox;
+  Shift? currentShift;
 
+  ShiftModel();
 
+  //----------------- Getter methods -----------------
 
-    //----------------- Database methods -----------------
+  DateTime get start => currentShift?.start ?? DateTime.now();
+  DateTime get end => currentShift?.end ?? DateTime.now();
+  Duration get duration => currentShift?.duration ?? Duration.zero;
+
+  //----------------- Setter methods -----------------
+
+  set shift(Shift shift) {
+    currentShift = shift;
+    log('shiftModel: currentshift set to $shift');
+    notifyListeners();
+  }
+
+  set start(DateTime start) {
+    currentShift?.start = start;
+    log('shiftModel: start set to $start');
+    notifyListeners();
+  }
+
+  set end(DateTime end) {
+    currentShift?.end = end;
+    log('shiftModel: end set to $end');
+    notifyListeners();
+  }
+
+  //----------------- Database methods -----------------
 
   /// initialize box for shift objects 
   Future<void> initialize() async {
@@ -27,27 +51,61 @@ class ShiftModel extends ChangeNotifier {
     });
   }
 
-  Future<void> saveCurrentshift() async {
-    if (currentshift == null) {
+  /// save current shift to shiftbox, only needed for flexible/individual shifts
+  Future<void> saveCurrentShift() async {
+    if (currentShift == null) {
       log('shiftModel: currentshift is null');
       return;
     } 
-    await _shiftBox.add(currentshift!);
+    await _shiftBox.add(currentShift!);
     notifyListeners(); 
   }
 
-  Future<void> updateshift(int index, Shift updatedshift) async {
-    await _shiftBox.putAt(index, updatedshift); 
+  /// save current shift to scheduledshiftbox and assign assistant via ID
+  Future<void> saveCurrentShiftAsScheduledShift(assistantID) async {
+    if (currentShift == null) {
+      log('shiftModel: currentshift is null');
+      return;
+    } 
+    await _scheduledShiftBox.add(ScheduledShift(currentShift!.start, currentShift!.end, assistantID));
     notifyListeners(); 
   }
 
-  List<Shift> getAllshifts() {
-    return _shiftBox.values.toList();
+  /// update shift at corresponding hive box depending on type
+  Future<void> updateShift(Shift updatedShift) async {
+    if (currentShift != null){
+      if (updatedShift is ScheduledShift) {
+        await _scheduledShiftBox.putAt(currentShift!.key, updatedShift);
+        } else {
+          await _shiftBox.putAt(currentShift!.key, updatedShift);
+        }
+        log('shiftModel: shift updated in database at index ${currentShift!.key}');
+        notifyListeners(); 
+    }
+    else {
+      log('shiftModel: currentshift is null');
+    }      
   }
 
-  Future<void> deleteshift() async {
-    if (currentshift != null) {
-      await _shiftBox.delete(currentshift!.key);
+  /// get shifts from shiftboxes 
+  List<Shift> getUnscheduledShiftsByDayRange(DateTime start, DateTime end) {
+  return _shiftBox.values.where(
+          (shift) => shift.start.day >= start.day && shift.end.day <= end.day).toList();
+  }
+  List<Shift> getScheduledShiftsByDayRange(DateTime start, DateTime end) {
+  return _scheduledShiftBox.values.where(
+          (shift) => shift.start.day >= start.day && shift.end.day <= end.day).toList();
+  }
+
+  /// delete current shift from corresponding hive box, depending on type
+  Future<void> deleteShift() async {
+    if (currentShift != null) {
+      if (currentShift! is ScheduledShift) {
+        await _scheduledShiftBox.delete(currentShift!.key);
+      } else {
+        await _shiftBox.delete(currentShift!.key);
+      }
+      log('shiftModel: shift deleted from database at index ${currentShift!.key}');
       notifyListeners();
     } else {
       log('shiftModel: currentshift is null');
