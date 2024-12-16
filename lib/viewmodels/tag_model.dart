296e00c76e9903b975e118beebce8bac3886c5f3
Annika <annika.lenneper@smail.th-koeln.de>
2024-12-16 20:18:16 +0000

    remove unused database methods and simplify tag management

--- lib/viewmodels/tag_model.dart
+++ lib/viewmodels/tag_model.dart
@@ -2,14 +2,10 @@ import 'dart:developer';
 import 'package:der_assistenzplaner/models/tag.dart';
 import 'package:der_assistenzplaner/views/shared/small_custom_widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-
 
 class TagModel extends ChangeNotifier {
   Tag? selectedTag;
-  late Box<Tag> _tagBox;
   late List<Tag> personalTags = [];
 
   get name => selectedTag?.name;
@@ -44,59 +40,15 @@ class TagModel extends ChangeNotifier {
   //----------------- User interaction methods -----------------
   
 
-  void selectTag(Tag tag) {
-    selectedTag = tag;
-  } 
-
-  void addTag(index) {
+  void addToPersonalTags(index) {
     personalTags.add(Tag(name, tagSymbol));
   }
 
-  void removeTag(Tag tag) {
+  void removeFromPersonalTags(Tag tag) {
     personalTags.remove(tag);
   }
 
   List<Widget> exampleTagsViewList() => exampleTags.map((tag)=>TagWidget(tag)).toList();
   List<Widget> personalTagsViewList() => personalTags.map((tag)=>TagWidget(tag)).toList();
 
-
-  //----------------- Database methods -----------------
-
-  Future<void> initialize() async {
-    _tagBox = await Hive.openBox<Tag>('tagBox');
-  
-    personalTags = getAllTags();
-  
-  /// listen to changes in database and update tag list accordingly
-    _tagBox.watch().listen((event) {
-      personalTags = getAllTags();
-      notifyListeners(); 
-      log('TagModel: tag list updated');
-    });
-  }
-
-  Future<void> saveCurrentAssistant() async {
-    if (selectedTag == null) {
-      log('AssistantModel: currentAssistant is null');
-      return;
-    } 
-    if (personalTags.contains(selectedTag)) {
-      log('AssistantModel: currentAssistant already exists in database');
-      return;
-    } 
-    await _tagBox.add(selectedTag!);
-    notifyListeners(); 
-  }
-
-  List<Tag> getAllTags() {
-    return _tagBox.values.toList();
-  }
-
-  Future<void> deleteTag(int index) async {
-    await _tagBox.deleteAt(index);
-    notifyListeners(); 
-  }
-
-
-  
 }
