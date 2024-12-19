8945b3b1feab9646c4054ef2fbddb6660760a122
Annika <annika.lenneper@smail.th-koeln.de>
2024-12-19 20:53:54 +0000

    refactor WorkscheduleModel to update shift display options and remove ScheduledShift references

--- lib/viewmodels/workschedule_model.dart
+++ lib/viewmodels/workschedule_model.dart
@@ -1,41 +1,36 @@
 import 'package:der_assistenzplaner/models/shift.dart';
-import 'package:der_assistenzplaner/models/workschedule.dart';
 import 'package:der_assistenzplaner/viewmodels/shift_model.dart';
 import 'package:flutter/material.dart';
-import 'package:der_assistenzplaner/test_data.dart';
 import 'package:provider/provider.dart';
 
 
-enum DisplayShifts {scheduled, upcoming, scheduledAndUpcoming, assistant}
+enum ShiftDisplayOptions {scheduled, unscheduled, all, assistant}
 
 /// used for displaying shifts in calendar generating work schedule and export functions
 /// needs no database, uses lists from scheduledShiftModel
 class WorkscheduleModel extends ChangeNotifier {
-  Workschedule workschedule = createTestWorkSchedule();
+  var _selectedDisplayOption = ShiftDisplayOptions.scheduled;
 
-  WorkscheduleModel();
+  void updateDisplayOption(ShiftDisplayOptions option) {
+    _selectedDisplayOption = option;
+    notifyListeners();
+  }
 
-  get start => workschedule.start;
-  get end => workschedule.end;
-
-  //test data, remove later
-  List<ScheduledShift> get scheduledShifts => workschedule.scheduledShifts;
+  ShiftDisplayOptions get selectedDisplayOption => _selectedDisplayOption;
 
   /// returns list of scheduled shifts, depending on selected display option
   /// hand over function to calendar event loader to display selected shifts
-  List<ScheduledShift> selectDisplayedShifts (context, DisplayShifts selected, {String? assistantID}) {
+  List<Shift> selectDisplayedShifts (context, ShiftDisplayOptions selected, {String? assistantID}) {
     late ShiftModel shiftModel = Provider.of<ShiftModel>(context, listen: false);
     switch (selected) {
-      case DisplayShifts.scheduled:
+      case ShiftDisplayOptions.scheduled:
         return shiftModel.scheduledShifts;
-      case DisplayShifts.upcoming:
-        return shiftModel.upcomingShifts as List<ScheduledShift>;
-      case DisplayShifts.scheduledAndUpcoming:
-        return shiftModel.scheduledAndUpcomingShifts;
-      case DisplayShifts.assistant:
-        return shiftModel.shiftsByAssistantsMap[assistantID] ?? [];
+      case ShiftDisplayOptions.unscheduled:
+        return shiftModel.unscheduledShifts;
+      case ShiftDisplayOptions.all:
+        return shiftModel.shifts;
+      case ShiftDisplayOptions.assistant:
+        return shiftModel.getMapOfShiftsByAssistants()[assistantID] ?? [];
     }
   }
-
-
 }
