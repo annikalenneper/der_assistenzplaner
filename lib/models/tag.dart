41b0beeaff36a1cb048f38ef7dbaccd2ee94b2ab
Annika <annika.lenneper@smail.th-koeln.de>
2024-12-19 20:47:59 +0000

    remove hive annotation and adapter for tags

--- lib/models/tag.dart
+++ lib/models/tag.dart
@@ -1,20 +1,12 @@
 import 'package:der_assistenzplaner/models/shift.dart';
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
 
-part 'tag.g.dart'; 
-
-@HiveType(typeId: 4) 
 class Tag {
-  @HiveField(0)
   String name;
-
-  @HiveField(1)
   IconData tagSymbol;
 
   Tag(this.name, this.tagSymbol);
 }
-
 
 class ShiftTag {
   final Tag tag;
