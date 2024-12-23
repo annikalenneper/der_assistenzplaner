ab1f13af4fe1443dd91f4a4f05b0a0127c9356f2
Annika <annika.lenneper@smail.th-koeln.de>
2024-12-23 11:23:26 +0000

    remove AssistantAddView and update import paths in assistant_screen.dart

--- lib/views/assistant_screen.dart
+++ lib/views/assistant_screen.dart
@@ -1,7 +1,7 @@
 import 'package:der_assistenzplaner/utils/helper_functions.dart';
 import 'package:der_assistenzplaner/utils/step_data.dart';
 import 'package:der_assistenzplaner/views/settings_screen.dart';
-import 'package:der_assistenzplaner/views/shared/stepper.dart';
+import 'package:der_assistenzplaner/views/shared/user_input_widgets.dart';
 import 'package:der_assistenzplaner/views/shared/view_containers.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -46,7 +46,6 @@ class _AssistantPageState extends State<AssistantPage> {
                 changePageViewIndex: _setAssistantPageViewState
               ),
               AssistantDetailView(),
-              AssistantAddView()
             ],
           ),
         ),
@@ -113,7 +112,7 @@ class AssistantListView extends StatefulWidget {
                     return PopUpBox(
                       view:  DynamicStepper(
                         steps: addAssistantStepData(),
-                        onComplete: (inputs) => saveToDatabase(context, inputs, Type.assistant),
+                        onComplete: (inputs) => saveStepperInput(context, inputs, Type.assistant),
                         ),
                       );
                     }, 
@@ -226,125 +225,3 @@ class AssistantDetailView extends StatelessWidget {
   }
 }
 
-
-//----------------- AssistantAddView -----------------
-
-
-
-class AssistantAddView extends StatefulWidget {
-  const AssistantAddView({super.key});
-
-  @override
-  State<AssistantAddView> createState() => _AssistantAddViewState();
-}
-
-class _AssistantAddViewState extends State<AssistantAddView> {
-  int _index = 0;
-
-  /// controller for textfields
-  final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _hoursController = TextEditingController();
-
-  void _submitDataAndCreateAssistant() {
-    /// get textfield values withouth whitespaces
-    final name = _nameController.text.trim();
-    final hoursText = _hoursController.text.trim();
-    /// input check
-    if (name.isEmpty || hoursText.isEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Bitte gib einen Namen und eine monatliche Stundenanzahl für deine Assistenzkraft ein')),
-      );
-      return;
-    }
-    /// only accept double as input
-    final  double? hours = double.tryParse(hoursText);
-    if (hours == null) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Bitte gib eine gültige Stundenzahl ein')),
-      );
-      return;
-    }
-    /// save assistant to database
-    final assistantModel = Provider.of<AssistantModel>(context, listen: false);
-    assistantModel.currentAssistant = Assistant(name, hours);
-    assistantModel.saveCurrentAssistant();
-    log(assistantModel.assistants.toString());
-    /// navigate back to AssistantScreen with new data
-    Navigator.pop(context);
-
-    /// clear textfields
-    _nameController.clear();
-    _hoursController.clear();
-    setState(() {
-      _index = 0;
-    });
-  }
-
-  /// stepper for user input of assistant data 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Erstelle eine neue Assistenzkraft'),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            navigateToAssistantScreen(context);
-          },
-        ),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(20),
-        child: Stepper(
-          currentStep: _index,
-          onStepCancel: () {
-            if (_index > 0) {
-              setState(() {
-                _index -= 1;
-              });
-            }
-          },
-          onStepContinue: () {
-            if (_index < 1) {
-              setState(() {
-                _index += 1;
-              });
-            } else {
-              _submitDataAndCreateAssistant();
-              navigateToAssistantScreen(context);
-            }
-          },
-          onStepTapped: (int index) {
-            setState(() {
-              _index = index;
-            });
-          },
-          steps: <Step>[
-            Step(
-              title: const Text('Wie soll die neue Assistenzkraft heißen?'),
-              content: TextField(
-                controller: _nameController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Name der Assistenzkraft',
-                ),
-              ),
-            ),
-            Step(
-              title: const Text('Wie viele Stunden soll die Assistenzkraft arbeiten?'),
-              content: TextField(
-                controller: _hoursController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Stundenzahl pro Monat',
-                ),
-                //only numbers allowed
-                keyboardType: TextInputType.number, 
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
