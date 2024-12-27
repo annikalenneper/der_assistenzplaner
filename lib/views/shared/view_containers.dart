b77e8113556129982b811e8b2be0ef26c049679d
Annika <annika.lenneper@smail.th-koeln.de>
2024-12-27 20:24:47 +0000

    refactor SettingsBox to accept a list of widgets for settings and improve layout in PopUpBox

--- lib/views/shared/view_containers.dart
+++ lib/views/shared/view_containers.dart
@@ -1,9 +1,13 @@
+import 'package:der_assistenzplaner/views/shared/user_input_widgets.dart';
 import 'package:flutter/material.dart';
 
+
+/// displays multiple dropDownMenus for corresponding settings
 class SettingsBox extends StatelessWidget {
   final String title;
-  final Widget? settings;
-  const SettingsBox({super.key, required this.title, this.settings});
+  final List<Widget> settings;
+
+  const SettingsBox({super.key, required this.title, required this.settings});
 
   @override
   Widget build(BuildContext context) {
@@ -11,16 +15,29 @@ class SettingsBox extends StatelessWidget {
       height: 100,
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Align(
-          alignment: Alignment.topLeft,
-          child: Text(title),
-        )
-      )
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: Text(title),
+            ),
+            const SizedBox(height: 10),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(
+                children: settings.map((setting) {
+                  return Expanded(child: setting);
+                }).toList(),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
-
+/// generic pop up window 
 class PopUpBox extends StatelessWidget {
   final Widget view;
   const PopUpBox({super.key, required this.view});
@@ -30,8 +47,8 @@ class PopUpBox extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(20.0),
       child: SizedBox(
-        height: MediaQuery.of(context).size.height * 0.8,
-        width: MediaQuery.of(context).size.width * 0.8,
+        height: MediaQuery.of(context).size.height * 0.5,
+        width: MediaQuery.of(context).size.width * 0.5,
         child: view
       ),
     );
