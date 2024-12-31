37defbe88f09ff1b8d3cf85974fb4bd20d374849
Annika <annika.lenneper@smail.th-koeln.de>
2024-12-31 14:06:08 +0000

    move database methods to repository, remove _assistantBox

--- lib/viewmodels/assistant_model.dart
+++ lib/viewmodels/assistant_model.dart
@@ -1,13 +1,10 @@
 import 'dart:developer';
 import 'package:der_assistenzplaner/data/models/assistant.dart';
 import 'package:der_assistenzplaner/data/repositories/assistant_repository.dart';
-import 'package:der_assistenzplaner/utils/shared_preferences_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:der_assistenzplaner/data/models/tag.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
 class AssistantModel extends ChangeNotifier {
-  late Box<Assistant> _assistantBox;
   AssistantRepository assistantRepository = AssistantRepository();
   List<Assistant> assistants = [];
   Map<String, Assistant> assistantMap = {}; /// effizienter Zugriff auf Assistenten per ID (O(1) statt O(n))
@@ -18,6 +15,7 @@ class AssistantModel extends ChangeNotifier {
 
   //----------------- Getter methods -----------------
 
+  String get assistantID => currentAssistant?.assistantID ?? '';
   String get name => currentAssistant?.name ?? '';
   double get contractedHours => currentAssistant?.contractedHours ?? 0.0;
   double get actualHours => currentAssistant?.actualHours ?? 0.0;
@@ -64,66 +62,43 @@ class AssistantModel extends ChangeNotifier {
   
 
 
-  ///TO-DO: implement removeTag(Tag tag) method
+
 
 
   //----------------- Data Manipulation Methods -----------------
 
 
-  Future<void> saveNewAssistant(Assistant newAssistant) async {
-    await assistantRepository.saveNewAssistant(newAssistant);
-    log('Assistant saved to database: $newAssistant');
+  Future<void> saveAssistant(Assistant assistant) async {
+    await assistantRepository.saveAssistant(assistant);
     notifyListeners(); 
   }
 
+  /// color assignment saved in SharedPreferences, independent from assistant object
   Future<void> assignColor(String assistentID, Color color) async {
     assistantColorMap[assistentID] = color;
     await assistantRepository.saveAssistantColor(assistentID, color);
-    log('AssistantModel: assigned color $color to assistant $assistentID');
+    notifyListeners();
+  }
+
+  Future<void> assignTag(String assistantID, Tag tag) async {
+    if (assistantMap.containsKey(assistantID)) {
+    assistantMap[assistantID]!.tags.add(tag);
+    await assistantRepository.saveAssistant(assistantMap[assistantID]!);
+    notifyListeners();
+    } else {
+      log('AssistantModel: assignTag: assistantID $assistantID not found');
+    }
+  } 
+
+  Future<void> deleteAssistant(String assistantID) async {
+    await assistantRepository.deleteAssistant(assistantID);
     notifyListeners();
   }
   
-  void addNote(String title, String text) {
-    if (currentAssistant == null) {
-      log('currentAssistant is null');
-    } else {
-      currentAssistant!.notes.add(Note(title, text));
-      assistantRepository.saveAssistantNote(currentAssistant!.assistantID, currentAssistant!.notes.last);
-    }
-  } 
-    
-
-  void removeNotebyIndex(int index) {
-    if (currentAssistant == null) {
-      log('currentAssistant is null');
-    } else if (index < 0 || index >= currentAssistant!.notes.length) {
-      log('Index $index out of bounds for notes list.');
-    } else {
-      currentAssistant!.notes.removeAt(index);
-    } 
-  }
-
-  void assignTag(Tag tag) {
-    if (currentAssistant == null) {
-      log('currentAssistant is null');
-    } else if (currentAssistant!.tags.contains(tag)) {
-      log('Tag $tag bereits zugeordnet.');
-    } else {
-      currentAssistant!.tags.add(tag);
-    }
-  } 
-
-  Future<void> deleteAssistant() async {
-    if (currentAssistant != null) {
-      await assistantRepository.deleteAssistant(currentAssistant!.assistantID);
-      notifyListeners();
-    } else {
-      log('AssistantModel: currentAssistant is null');
-    }
-  }
 
   //----------------- Initializaton Methods -----------------
 
+  /// loads all assistants from database and assigns them to the assistantMap, creates a map of assistantID to color
   Future<void> initialize() async {
     _loadAssistants();
     _loadAssistantColors();
