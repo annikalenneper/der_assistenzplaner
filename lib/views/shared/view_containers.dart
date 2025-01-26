6bec61454c587a6e699d247e3cb4ff3239188cb1
Annika <annika.lenneper@smail.th-koeln.de>
2025-01-26 21:03:23 +0000

    refactor: remove unused PopUpBox widget from view_containers.dart

--- lib/views/shared/view_containers.dart
+++ lib/views/shared/view_containers.dart
@@ -37,20 +37,3 @@ class SettingsBox extends StatelessWidget {
   }
 }
 
-/// generic pop up window 
-class PopUpBox extends StatelessWidget {
-  final Widget view;
-  const PopUpBox({super.key, required this.view});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height * 0.5,
-        width: MediaQuery.of(context).size.width * 0.5,
-        child: view
-      ),
-    );
-  }
-}
