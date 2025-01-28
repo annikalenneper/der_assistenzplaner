f676802e661febafbdd3caa666e3725da53c2b2d
Annika <annika.lenneper@smail.th-koeln.de>
2025-01-28 22:42:06 +0000

    refactor: restructure Settings components for improved readability and functionality

--- lib/views/settings/settings_viewcontainer.dart
+++ lib/views/settings/settings_viewcontainer.dart
@@ -1,42 +1,51 @@
 
+import 'package:der_assistenzplaner/viewmodels/settings_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-enum RowOrColumn { row, column }
 
-class Settings extends StatefulWidget {
-  final List<Widget> settings;
-  final RowOrColumn alignment = RowOrColumn.row;
-  final VoidCallback onSaved;
+class SettingsInfo extends StatelessWidget {
+  final String title;
+  final String info;
+  final String selectedValue;
 
-  Settings({super.key, required this.settings, alignment, required this.onSaved});
+  const SettingsInfo({super.key, required this.title, required this.info, required this.selectedValue});
 
-  @override
-  State<Settings> createState() => SettingsState();
-}
-
-class SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          children: widget.settings,
-        ),
-        ElevatedButton(
-          onPressed: widget.onSaved,
-          child: const Text('Speichern'),
-        ),
-      ],
+    return Consumer<SettingsModel>(
+      builder: (context, settings, child) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Flexible(
+              flex: 1,
+              child: Text('Platzhalter')
+            ),
+            Flexible(
+              flex: 2,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(title),
+                  Text(info),
+                ],
+              ),
+            ),
+            Text(selectedValue),
+          ],
+        );
+      },
     );
   }
 }
 
-/// displays multiple dropDownMenus for corresponding settings
-class SettingsBox extends StatelessWidget {
-  final String title;
-  final List<Widget> settings;
 
-  const SettingsBox({super.key, required this.title, required this.settings});
+class SettingsSelection extends StatelessWidget {
+  final List<Widget> settings;
+  final VoidCallback onButtonClick;
+
+  const SettingsSelection({super.key, required this.settings, required this.onButtonClick});
 
   @override
   Widget build(BuildContext context) {
@@ -46,16 +55,18 @@ class SettingsBox extends StatelessWidget {
         child: Column(
           children: [
             Align(
-              alignment: Alignment.topLeft,
-              child: Text(title),
-            ),
-            const SizedBox(height: 10),
-            Align(
               alignment: Alignment.bottomCenter,
               child: Row(
                 children: settings.map((setting) {
                   return Expanded(child: setting);
                 }).toList(),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: onButtonClick,
+                child: const Text('Speichern'),
               ),
             ),
           ],
