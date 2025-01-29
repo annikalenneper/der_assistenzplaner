0545df7e020a42b055036da79668d6eb8a8b4456
Annika <annika.lenneper@smail.th-koeln.de>
2025-01-29 01:31:21 +0000

    refactor: improve layout and structure of SettingsInfo widget for better readability and interaction

--- lib/views/settings/settings_viewcontainer.dart
+++ lib/views/settings/settings_viewcontainer.dart
@@ -3,76 +3,59 @@ import 'package:der_assistenzplaner/viewmodels/settings_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-
-class SettingsInfo extends StatelessWidget {
+class SettingsInfo extends StatefulWidget {
   final String title;
   final String info;
   final String selectedValue;
 
-  const SettingsInfo({super.key, required this.title, required this.info, required this.selectedValue});
+  const SettingsInfo({
+    super.key, 
+    required this.title, 
+    required this.info, 
+    required this.selectedValue, 
+  });
+
+   
+  @override
+  State<StatefulWidget> createState() => SettingsInfoState();
+}
+
+class SettingsInfoState extends State<SettingsInfo> {
+  SettingsInfoState();
 
   @override
   Widget build(BuildContext context) {
     return Consumer<SettingsModel>(
       builder: (context, settings, child) {
-        return Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Flexible(
-              flex: 1,
-              child: Text('Platzhalter')
+        return Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(widget.title),
+                SizedBox(width: 60),
+                Text(widget.info),     
+                Spacer(),     
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Row(
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.edit),
+                        onPressed: () => {},
+                      ),
+                      SizedBox(width: 5),
+                      Text(widget.selectedValue),             
+                    ]
+                  )
+                ),
+              ],
             ),
-            Flexible(
-              flex: 2,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(title),
-                  Text(info),
-                ],
-              ),
-            ),
-            Text(selectedValue),
-          ],
+          ),
         );
       },
     );
   }
 }
-
-
-class SettingsSelection extends StatelessWidget {
-  final List<Widget> settings;
-  final VoidCallback onButtonClick;
-
-  const SettingsSelection({super.key, required this.settings, required this.onButtonClick});
-
-  @override
-  Widget build(BuildContext context) {
-    return Flexible(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Row(
-                children: settings.map((setting) {
-                  return Expanded(child: setting);
-                }).toList(),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                onPressed: onButtonClick,
-                child: const Text('Speichern'),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
