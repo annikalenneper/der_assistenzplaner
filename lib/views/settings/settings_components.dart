1e282d37be696e44cbcb8ab594ff3b457e17c299
Annika <annika.lenneper@smail.th-koeln.de>
2025-02-15 15:40:42 +0000

    feat: add introductory text and new SettingsBox widget for improved settings layout

--- lib/views/settings/settings_components.dart
+++ lib/views/settings/settings_components.dart
@@ -3,6 +3,16 @@ import 'package:der_assistenzplaner/viewmodels/settings_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+
+final settingsIntroText = "Deine Auswahl legt fest, wie deine Schichten im Kalender eingetragen werden. \nDu kannst jederzeit einzelne Schichten im Kalender anpassen oder deine Einstellungen ändern.";
+
+final frequencyTitle = "Frequenz";
+final weekdayTitle = "Wochentage";
+final timeTitle = "Schichtzeit";
+final dueDateTitle = "Verfügbarkeiten";
+
+
+
 class Settings extends StatefulWidget {
   final String title;
   final String selectedValue;
@@ -34,7 +44,6 @@ class SettingsState extends State<Settings> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(widget.title, style: TextStyle(fontSize: 20)),
-                Spacer(),     
                 Align(
                   alignment: Alignment.bottomLeft,
                   child: Row(
@@ -53,6 +62,32 @@ class SettingsState extends State<Settings> {
           ),
         );
       },
+    );
+  }
+}
+
+class SettingsBox extends StatelessWidget {
+  final String category;
+  final List<Settings> children;
+
+  const SettingsBox({super.key, required this.category, required this.children});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: ListView(
+        children: [
+          Text(category, style: TextStyle(fontSize: 24)),
+          Column(
+            children: children,
+          ),
+        ],
+      ),
     );
   }
 }
